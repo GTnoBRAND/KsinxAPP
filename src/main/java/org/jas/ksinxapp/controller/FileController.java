2fbecd44ebec5958f3a4d28e045a9d82e87a4086
gtnobrand <jaskaamanbayev2003@gmail.com>
2026-06-20 15:34:06 +0500

    ...

--- src/main/java/org/jas/ksinxapp/controller/FileController.java
+++ src/main/java/org/jas/ksinxapp/controller/FileController.java
@@ -1,6 +1,7 @@
 package org.jas.ksinxapp.controller;
 
-import org.jas.ksinxapp.service.FileStorageService;
+import lombok.RequiredArgsConstructor;
+import org.jas.ksinxapp.service.MinIoStorageService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,18 +14,15 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/files")
+@RequiredArgsConstructor
 public class FileController {
 
-    private final FileStorageService service;
-
-    public FileController(FileStorageService service) {
-        this.service = service;
-    }
+    private final MinIoStorageService minIoStorageService;
 
     @PostMapping("/upload")
     public ResponseEntity<Map<String, String>> uploadFile(@RequestParam MultipartFile file) {
 
-        String fileDownloadUri = service.storeFile(file);
+        String fileDownloadUri = minIoStorageService.publicUpload(file);
 
         //return a simple json object containing a enw url
         Map<String, String> response = new HashMap<>();
