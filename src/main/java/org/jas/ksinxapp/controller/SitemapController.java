fe7237835c9bf39f92ee686acfcff1200f8691d7
gtnobrand <jaskaamanbayev2003@gmail.com>
2026-06-29 22:33:29 +0500

    rendering directly without view name

--- src/main/java/org/jas/ksinxapp/controller/SitemapController.java
+++ src/main/java/org/jas/ksinxapp/controller/SitemapController.java
@@ -6,23 +6,28 @@ import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.thymeleaf.context.Context;
+import org.thymeleaf.spring6.SpringTemplateEngine;
 
 @Controller
 public class SitemapController {
 
     private final CourseService service;
+    private final SpringTemplateEngine templateEngine;
     private final String baseUrl;
 
-    public SitemapController(CourseService service, @Value("${app.base-url}") String baseUrl){
+    public SitemapController(CourseService service, SpringTemplateEngine templateEngine,@Value("${app.base-url}") String baseUrl){
         this.service = service;
+        this.templateEngine = templateEngine;
         this.baseUrl = baseUrl;
     }
 
     @GetMapping(value = "/sitemap.xml", produces = MediaType.APPLICATION_XML_VALUE)
-    public String sitemap(Model model){
-        model.addAttribute("baseUrl", baseUrl);
-        model.addAttribute("courses", service.findActiveCourseForSitemap());
-        return "sitemap";
+    public String sitemap(){
+        Context context = new Context();
+        context.setVariable("baseUrl", baseUrl);
+        context.setVariable("courses", service.findActiveCourseForSitemap());
+        return templateEngine.process("sitemap",context);
     }
 
 }
