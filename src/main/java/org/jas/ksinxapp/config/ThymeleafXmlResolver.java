e93b06d5378de034805007ca116a0737fdc96b77
gtnobrand <jaskaamanbayev2003@gmail.com>
2026-06-30 10:45:17 +0500

    changed prefix in xml resolver

--- src/main/java/org/jas/ksinxapp/config/ThymeleafXmlResolver.java
+++ src/main/java/org/jas/ksinxapp/config/ThymeleafXmlResolver.java
@@ -1,5 +1,6 @@
 package org.jas.ksinxapp.config;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -9,18 +10,22 @@ import org.thymeleaf.templatemode.TemplateMode;
 import java.util.Set;
 
 @Configuration
+@Slf4j
 public class ThymeleafXmlResolver {
 
     @Bean
     public SpringResourceTemplateResolver xmlTemplateResolver(ApplicationContext applicationContext){
         SpringResourceTemplateResolver resolver = new SpringResourceTemplateResolver();
         resolver.setApplicationContext(applicationContext);
-        resolver.setPrefix("classpath:/static/");
+        resolver.setPrefix("classpath:/templates/");
         resolver.setSuffix(".xml");
         resolver.setTemplateMode(TemplateMode.XML);
         resolver.setCharacterEncoding("UTF-8");
         resolver.setResolvablePatterns(Set.of("sitemap"));
         resolver.setOrder(1);
+        System.out.println("XML TEMPLATE RESOLVER REGISTERED: prefix=" + resolver.getPrefix()
+                + ", suffix=" + resolver.getSuffix()
+                + ", patterns=" + resolver.getResolvablePatterns());
         return resolver;
     }
 }
