aa697114f6363913cd1cecb6ea784df8997f420d
gtnobrand <jaskaamanbayev2003@gmail.com>
2026-07-01 16:03:38 +0500

    added manual @ResponseBody to treat the response not as a view

--- src/main/java/org/jas/ksinxapp/controller/SitemapController.java
+++ src/main/java/org/jas/ksinxapp/controller/SitemapController.java
@@ -4,8 +4,8 @@ import org.jas.ksinxapp.service.CourseService;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.thymeleaf.context.Context;
 import org.thymeleaf.spring6.SpringTemplateEngine;
 
@@ -23,6 +23,7 @@ public class SitemapController {
     }
 
     @GetMapping(value = "/sitemap.xml", produces = MediaType.APPLICATION_XML_VALUE)
+    @ResponseBody
     public String sitemap(){
         Context context = new Context();
         context.setVariable("baseUrl", baseUrl);
